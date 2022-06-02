4ceb3efaf14c1949df56ddfb25f45ef2553edd07
Kachy Akamnonu <kachyk@gmail.com>
2022-06-02 14:55:29 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /**
- * Main entry point 
- *
- *
- *
- */
-
-int main (void)
+ *  *  * main - Entry point
+ *   *   *
+ *    *    * Return: Always 0 (Success)
+ *     *     */
+ int main(void)
 {
-	puts ("\"Programming is like building a multilungual puzzle");
-	return (0);
-
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
