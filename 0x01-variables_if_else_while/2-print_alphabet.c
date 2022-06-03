2a4c6f03cd2c046b4f903fb01d86cef78b304369
Kachy Akamnonu <kachyk@gmail.com>
2022-06-03 11:59:31 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,17 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
 int main(void)
 {
 		char k;
-		for (k="a"; k<="z"; k++)
+
+		for (k= 'a'; k <= 'z'; k++)
 		putchar(k);
-		putchar("\n");
+
+		putchar('\n');
 		/* your code goes there */
 		return (0);
 }
