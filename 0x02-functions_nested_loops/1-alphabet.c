76d1be2abbc9ba6c162df1b5af72ad94b54915e9
Kachy Akamnonu <kachyk@gmail.com>
2022-06-07 15:31:22 -0700

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h":
+/**
+ * * print_alphabet prints lowercase chars
+ *
+ * */
+
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+
+	{
+		_putchar(alpha);
+		alpha++;
+
+	}
+
+	_putchar('\n');
+
+}
