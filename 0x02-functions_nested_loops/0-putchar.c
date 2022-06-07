070d4be94686201342b852bcd7cf80ec85052764
Kachy Akamnonu <kachyk@gmail.com>
2022-06-07 15:12:04 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Entry
+ * 
+ * 
+ * Return:0 
+ */
+
+int main(void)
+{
+	char word[9] = "_putchar";
+
+	int k = 0;
+
+	while (k < 8)
+	{
+		_putchar(word[k]);
+		k++;
+
+	}
+	_putchar('\n'');
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+You Better Read me 
