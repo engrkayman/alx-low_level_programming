b363a340dcc6b58f08625f817ff4724b8e19cb0e
Kachy Akamnonu <kachyk@gmail.com>
2022-06-08 04:06:01 -0700

    YES

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 /**
- * * print_alphabet >> prints lowercase chars
+ * * print_alphabet >> prints lowercase alphabets
  *
  * */
 
@@ -13,8 +12,7 @@ void print_alphabet(void)
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 
 	{
-		_putchar(alpha);
-		
+		_putchar(alpha);	
 	}
 
 	_putchar('\n');
