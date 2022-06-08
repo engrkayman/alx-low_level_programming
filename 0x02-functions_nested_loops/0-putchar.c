d1b05b6f0a796ab52d236c8bb7b8c2d6e06ff58a
Kachy Akamnonu <kachyk@gmail.com>
2022-06-08 03:44:27 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - Entry
- * 
+ *
  *
  * Return:0
  */
@@ -16,6 +16,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
